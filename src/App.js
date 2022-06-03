02ff5ff81505739d3328f846f1690a5a610a808c
unknown <brendencyr24@gmail.com>
2022-06-03 00:28:40 -0700

    before the bug

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Board from './components/Board';
+import Keyboard from './components/Keyboard';
+// import {createContext, useState} from "react";
+// import { boardDefault } from './components/Words';
+
+// export const AppContext = createContext()
 
 function App() {
+  // const [board, setBoard] = useState(boardDefault)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+      Wordle
+      </nav>
+      
+        <Board />
+        <Keyboard />
+      
     </div>
   );
 }
 
 export default App;
+
+// <AppContext.Provider value={{board, setBoard}}></AppContext.Provider>
+// </AppContext.Provider>
---
+++ src/components/Letter.js
@@ -0,0 +1,11 @@
+import React  from 'react';
+// import {AppContext} from "../App";
+// {useContext}
+
+function Letter({letterPos, attemptVal}) {
+    // const {board} = useContext(AppContext)
+    // const letter = board[attemptVal][letterPos]
+    return <div className="letter">L</div>
+}
+
+export default Letter
